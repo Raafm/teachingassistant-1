55512162ab146e91e17a801d118ca716192aa7db
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:26:56 -0300

    refatoracao do metodo gravar

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -4,11 +4,14 @@ export class AlunoService {
   alunos: Aluno[] = [];
   
   gravar(aluno: Aluno): Aluno {
-    var result = new Aluno();
-    if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
+    var result = new Aluno();;
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
     return result;
+  }
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
   }
 }
